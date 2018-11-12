ec34bc3c24d5f1bc08cbfaa7613b2785b8104cef
Florian Marending <marending.florian@gmail.com>
2018-11-12 16:43:29 +0100

    Implement random grazing

--- src/cow.rs
+++ src/cow.rs
@@ -1,9 +1,37 @@
+use nannou::prelude::*;
+
+#[derive(Copy, Clone, Debug)]
+pub enum Move {
+    UP,
+    DOWN,
+    LEFT,
+    RIGHT,
+}
+
 pub struct Cow {
-    pub loc: (usize, usize),
+    pub loc: usize,
+    pub score: usize,
+    next_move: Option<Move>,
 }
 
 impl Cow {
-    pub fn new(x: usize, y: usize) -> Self {
-        Self { loc: (x, y) }
+    pub fn new(loc: usize) -> Self {
+        Self { loc: loc, score: 0, next_move: None }
+    }
+
+    pub fn compute_move(&mut self) {
+        let mv = match random_range(0, 4) {
+            0 => Move::UP,
+            1 => Move::DOWN,
+            2 => Move::LEFT,
+            3 => Move::RIGHT,
+            _ => panic!("rand error"),
+        };
+
+        self.next_move = Some(mv);
+    }
+
+    pub fn get_move(&self) -> Move {
+        self.next_move.unwrap()
     }
 }
--- src/field.rs
+++ src/field.rs
@@ -2,6 +2,7 @@ use nannou::prelude::*;
 use nannou::draw::Draw;
 
 use cow::Cow;
+use cow::Move;
 
 pub struct Field {
     cows: Vec<Cow>,
@@ -12,18 +13,18 @@ pub struct Field {
     height: f32,
 
     freeze: bool,
+    last_step: f32,
 }
 
 impl Field {
     pub fn new(width: f32, height: f32, size: usize) -> Self {
-        Self { cows: Vec::new(), patches: vec![0; size*size], size: size, width: width, height: height, freeze: false }
+        Self { cows: Vec::new(), patches: vec![0; size*size], size: size, width: width, height: height, freeze: false, last_step: 0.0 }
     }
 
     pub fn init(&mut self, n: usize) {
-        for i in 0..n {
-            let x = random_range(0, self.size);
-            let y = random_range(0, self.size);
-            self.add_cow(x, y);
+        for _ in 0..n {
+            let loc = random_range(0, self.size*self.size);
+            self.add_cow(loc);
         }
     }
 
@@ -36,8 +37,8 @@ impl Field {
         self.height = size.y;
     }
 
-    pub fn add_cow(&mut self, x: usize, y: usize) {
-        self.cows.push(Cow::new(x,y));
+    pub fn add_cow(&mut self, loc: usize) {
+        self.cows.push(Cow::new(loc));
     }
 
     pub fn draw(&self, draw: &Draw) {
@@ -60,7 +61,7 @@ impl Field {
 
         for c in self.cows.iter() {
             let radius = 5.0;
-            let (x,y) = c.loc;
+            let (x,y) = (c.loc % self.size, c.loc / self.size);
 
             let x = ((x as f32 + 0.5) / self.size as f32) * self.width - 0.5 * self.width;
             let y = ((y as f32 + 0.5) / self.size as f32) * self.height - 0.5 * self.height;
@@ -70,6 +71,35 @@ impl Field {
     }
 
     pub fn step(&mut self, dt: f32) {
+        let step_frequency = 2;
+        let step_interval = 1.0 / step_frequency as f32;
+        let grass_regen = 20;
+
+        self.last_step += dt;
+
+        if self.last_step < step_interval {
+            return;
+        }
+        self.last_step = 0.0;
+
+        // Move cows
+        for c in &mut self.cows {
+            c.compute_move();
+        }
+
+        for c in &mut self.cows {
+            move_cow(c, self.size);
+        }
+
+        // Eat grass
+        for c in &mut self.cows {
+            if self.patches[c.loc] == 0 {
+                c.score += 1;
+                self.patches[c.loc] = grass_regen;
+            }
+        }
+
+        // Recover grass
         for p in self.patches.iter_mut() {
             if *p > 0 {
                 *p -= 1;
@@ -77,4 +107,17 @@ impl Field {
         }
     }
 
+}
+
+fn move_cow(cow: &mut Cow, size: usize) {
+    let (mut x, mut y) = (cow.loc % size, cow.loc / size);
+
+    match cow.get_move() {
+        Move::UP => if y == size - 1 { y = 0 } else { y += 1 },
+        Move::DOWN => if y == 0 { y = size - 1 } else { y -= 1 },
+        Move::LEFT => if x == 0 { x = size - 1 } else { x -= 1 },
+        Move::RIGHT => if x == size - 1 { x = 0 } else { x += 1 },
+    }
+
+    cow.loc = y * size + x;
 }
