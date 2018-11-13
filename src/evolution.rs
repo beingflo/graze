a8fa535868b1126c517fe927e2c206ff755a462b
Florian Marending <floriama@student.ethz.ch>
2018-11-13 13:09:30 +0100

    Impl Mover for MutantCow

--- src/evolution.rs
+++ src/evolution.rs
@@ -2,8 +2,10 @@ use rand::Rng;
 
 use cow::Move;
 use field::Field;
+use traits::Mover;
 
 struct Evolver {
+
 }
 
 impl Evolver {
@@ -17,15 +19,35 @@ struct MutantCow {
     pub id: usize,
 }
 
-impl MutantCow {
-    pub fn new(loc: usize, id: usize) -> Self {
+impl Mover for MutantCow {
+    fn new(loc: usize, id: usize) -> Self {
         Self { loc: loc, score: 0, next_move: None, id: id }
     }
 
-    pub fn compute_move(&mut self, neighborhood: ([bool; 8], [bool; 8])) {
+    fn compute_move(&mut self, neighborhood: ([bool; 8], [bool; 8])) {
     }
 
-    pub fn get_move(&self) -> Move {
+    fn get_move(&self) -> Move {
         self.next_move.unwrap()
+    }
+
+    fn loc(&self) -> usize {
+        self.loc
+    }
+
+    fn set_loc(&mut self, loc: usize) {
+        self.loc = loc;
+    }
+
+    fn score(&self) -> usize {
+        self.score
+    }
+
+    fn inc_score(&mut self) {
+        self.score += 1;
+    }
+
+    fn id(&self) -> usize {
+        self.id
     }
 }
