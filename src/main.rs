7ffffe2e88a4dee8754cc7d956eab0d2317ba258
Florian Marending <floriama@student.ethz.ch>
2018-11-13 15:32:27 +0100

    Cleanup

--- src/main.rs
+++ src/main.rs
@@ -10,8 +10,6 @@ mod traits;
 use nannou::prelude::*;
 use nannou::event::SimpleWindowEvent;
 use ui::UserInterface;
-use field::Field;
-use cow::Cow;
 use evolution::Evolver;
 
 fn main() {
@@ -19,28 +17,26 @@ fn main() {
 }
 
 struct Model {
-    field: Field<Cow>,
     evolver: Evolver,
     ui: UserInterface,
 }
 
 fn model(app: &App) -> Model {
-    let _ = app.new_window().with_title("Graze").build().unwrap();
-    app.main_window().set_inner_size_points(720.0, 720.0);
-    let (width, height) = app.main_window().inner_size_points();
+    let _ = app.new_window().with_title("Graze - Evolved").build().unwrap();
+
+    let (width, height) = (720.0, 720.0);
+
+    app.main_window().set_inner_size_points(width, height);
 
     let ui = app.new_ui().build().unwrap();
     let ui = UserInterface::new(ui);
-
-    let mut field = Field::new(width, height, 50);
-    field.init(10);
 
     let mut evolver = Evolver::new(width, height, 50);
     evolver.field.init(10);
 
     evolver.evolve();
 
-    Model { field: field, evolver: evolver, ui: ui }
+    Model { evolver: evolver, ui: ui }
 }
 
 fn event(_: &App, mut model: Model, event: Event) -> Model {
@@ -48,17 +44,16 @@ fn event(_: &App, mut model: Model, event: Event) -> Model {
         Event::Update(update) => {
             let dt = update.since_last.secs() as f32;
 
-            //model.field.step(dt);
             model.evolver.step(dt);
             model.ui.update(dt);
         },
 
         Event::WindowEvent { simple: Some(SimpleWindowEvent::Resized(size)), .. } => {
-            model.field.update_size(size);
+            model.evolver.field.update_size(size);
         },
 
         Event::WindowEvent { simple: Some(SimpleWindowEvent::KeyPressed(nannou::VirtualKeyCode::Space)), .. } => {
-            model.field.toggle_freeze();
+            model.evolver.field.toggle_freeze();
         },
 
         _ => (),
@@ -69,12 +64,8 @@ fn event(_: &App, mut model: Model, event: Event) -> Model {
 fn view(app: &App, model: &Model, frame: Frame) -> Frame {
     let draw = app.draw();
     draw.background().color(WHITE);
-
-    //model.field.draw(&draw);
     model.evolver.field.draw(&draw);
-
     draw.to_frame(app, &frame).unwrap();
-    model.ui.ui.draw_to_frame(app, &frame).unwrap();
 
     frame
 }
